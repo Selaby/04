376c21d67c1da71ade5698203d19c78a0f16aa40
Selaby <oratnek.ihcuekat@gmail.com>
2021-05-24 21:41:29 +0900

    課題3が難航

--- pos-system.py
+++ pos-system.py
@@ -1,3 +1,7 @@
+import pandas as pd
+
+ITEM_MASTER_CSV_PATH="./master.csv"
+
 ### 商品クラス
 class Item:
     def __init__(self,item_code,item_name,price):
@@ -29,27 +33,45 @@ class Order:
                 print(f"商品コード:{m.item_code}")
                 print(f"商品名:{m.item_name}")
                 print(f"価格:{m.price}")
-    
-    
+
+# 課題3 csvから商品マスタを登録する
+def register_by_csv(csv_path):
+    item_master=[]
+    item_master_df = pd.read_csv(csv_path, encoding="utf-8",dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
+    for item_code,item_name,price in zip(item_master_df["item_code"],item_master_df["item_name"],item_master_df["price"]):
+        item_master.append(Item(item_code,item_name,price))
+    return item_master
+
 ### メイン処理
 def main():
     # マスタ登録
-    item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
+    # item_master=[]
+    # item_master.append(Item("001","りんご",100))
+    # item_master.append(Item("002","なし",120))
+    # item_master.append(Item("003","みかん",150))
+
+    # 課題3 csvから商品マスタを登録する
+    item_master=register_by_csv(ITEM_MASTER_CSV_PATH)
+    # item_master = pd.read_csv("./master.csv", encoding="utf-8")
+    # for item_code, item_name, price in zip(list(item_master["item_code"]),list(item_master["item_name"]),list(item_master["price"])):
+    #     item_master.append(Item(item_code, item_name, price))
+    print(item_master)
     
     # オーダー登録
     order=Order(item_master)
-    order.add_item_order("001")
-    order.add_item_order("002")
-    order.add_item_order("003")
+
+    # 課題2 ターミナルから商品コードを登録する
+    order_code = input("商品コードを入力してください >> ")
+    order.add_item_order(order_code)
+    # order.add_item_order("001")
+    # order.add_item_order("002")
+    # order.add_item_order("003")
     
     # オーダー表示
     # order.view_item_list()
 
     # 商品情報表示
-    order.view_name_and_price("001")
+    order.view_name_and_price(order_code)
         
 if __name__ == "__main__":
     main()
